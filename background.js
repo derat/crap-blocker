8da9508a8153c25b547cb3adc6cac6b0bd464b3b
Daniel Erat <dan@erat.org>
2021-12-31 07:21:10 -0400

    v2: Validate match patterns and accept hostnames.
    I accidentally added a pattern like '*.//*.example.com/*'
    (note '.' instead of ':' in scheme), which resulted in the
    chrome.webRequest.onBeforeRequest.addListener() call failing
    and nothing being blocked.

--- background.js
+++ background.js
@@ -3,26 +3,26 @@
 // found in the LICENSE file.
 
 function filter(details) {
-  return {cancel: true};
+  return { cancel: true };
 }
 
 function updateFilter() {
-  if (chrome.webRequest.onBeforeRequest.hasListener(filter))
+  if (chrome.webRequest.onBeforeRequest.hasListener(filter)) {
     chrome.webRequest.onBeforeRequest.removeListener(filter);
+  }
 
-  chrome.storage.sync.get('patterns', function(items) {
-    if (items.patterns) {
-      var patterns = items.patterns.split("\n");
-      console.log('updating filter to block ' + patterns.join(' '));
-      chrome.webRequest.onBeforeRequest.addListener(
-          filter, {urls: patterns}, ['blocking']);
-    }
+  chrome.storage.sync.get("patterns", (items) => {
+    if (!items.patterns) return;
+    const patterns = items.patterns.split("\n");
+    console.log(`Updating filter to block ${patterns.join(" ")}`);
+    chrome.webRequest.onBeforeRequest.addListener(filter, { urls: patterns }, [
+      "blocking",
+    ]);
   });
 }
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-  if (namespace == 'sync' && changes.patterns)
-    updateFilter();
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace == "sync" && changes.patterns) updateFilter();
 });
 
 updateFilter();
--- options.js
+++ options.js
@@ -2,37 +2,54 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function $(id) { return document.getElementById(id); }
+function $(id) {
+  return document.getElementById(id);
+}
+
+// (Very loosely) matches Chrome match patterns as described at
+// https://developer.chrome.com/docs/extensions/mv3/match_patterns/.
+const matchPatternRegexp = new RegExp(
+  "^" +
+    /([a-z]+|\*):\/\//.source + // scheme
+    /[-.0-9a-z*]+/.source + // host
+    /\/.*/.source // path
+);
+
+// Matches hostnames, e.g. 'example.com' or 'sub-domain.example.co.uk'.
+const hostRegexp = /^[-.0-9a-z]+$/;
 
 function saveOptions() {
-  var lines = $('patterns-textarea').value.split("\n");
-  var patterns = [];
+  const lines = $("patterns-textarea").value.split("\n");
+  const patterns = [];
   for (var i = 0; i < lines.length; i++) {
     var pattern = lines[i].trim();
-    if (pattern)
-      patterns.push(pattern);
-  }
-  var value = patterns.sort().join("\n");
+    if (!pattern) continue;
 
-  chrome.storage.sync.set({ patterns: value }, function(items) {
-    $('patterns-textarea').value = value;
-    $('save-button').disabled = true;
+    if (hostRegexp.test(pattern)) pattern = `*://*.${pattern}/*`;
+
+    if (matchPatternRegexp.test(pattern)) patterns.push(pattern);
+    else console.warn(`Skipping invalid pattern "${pattern}"`);
+  }
+  const value = patterns.sort().join("\n");
+  chrome.storage.sync.set({ patterns: value }, (items) => {
+    $("patterns-textarea").value = value;
+    $("save-button").disabled = true;
   });
 }
 
 function restoreOptions() {
-  chrome.storage.sync.get('patterns', function(items) {
-    $('patterns-textarea').value = items.patterns ? items.patterns : '';
-    $('save-button').disabled = true;
+  chrome.storage.sync.get("patterns", (items) => {
+    $("patterns-textarea").value = items.patterns || "";
+    $("save-button").disabled = true;
   });
 }
 
 function textareaChanged() {
-  $('save-button').disabled = false;
+  $("save-button").disabled = false;
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-$('save-button').addEventListener('click', saveOptions);
-$('patterns-textarea').addEventListener('change', textareaChanged);
-$('patterns-textarea').addEventListener('keyup', textareaChanged);
-$('patterns-textarea').addEventListener('paste', textareaChanged);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+$("save-button").addEventListener("click", saveOptions);
+$("patterns-textarea").addEventListener("change", textareaChanged);
+$("patterns-textarea").addEventListener("keyup", textareaChanged);
+$("patterns-textarea").addEventListener("paste", textareaChanged);
